5826b1646d0066cb834306203b7fd6734bf7dc33
mhasadi78 <mhasadi78@gmail.com>
2018-12-26 03:44:29 +0330

    std3,4,5 info updated

--- stdInfoGiven.js
+++ stdInfoGiven.js
@@ -11,3 +11,18 @@ std2 = {
     'lastName' : 'Zolfi',
     'nationalID' : 156237354
 }
+std3 = {
+    'firstName' : 'Mehran',
+    'lastName' : 'Latifi',
+    'nationalID' : 156237332
+}
+std4 = {
+    'firstName' : 'Arman',
+    'lastName' : 'Zoormand',
+    'nationalID' : 156237857
+}
+std5 = {
+    'firstName' : 'Milad',
+    'lastName' : 'Ganji',
+    'nationalID' : 156237325
+}
