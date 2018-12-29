ec640037ebe349e43bb82bb73a89a60b1cc101a0
mhasadi78 <mhasadi78@gmail.com>
2018-12-29 16:15:55 +0330

    installed npm packages : hbs, express, body-prser . some changes in server.js code. changes in view directory : index.hbs renamed to form.hbs and new index.hbs created.

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log('server is runnig')
+console.log('server is runnig');
+// import required packages
+const fs = require('fs');
+const hbs = require('hbs');
+const express = require('express')
+const bodyParser = require('body-parser')
+var app = express()
+// using hbs for view (appearance of program)
+app.set('view engine', 'hbs')
+// 
+app.use(bodyParser.urlencoded({extended : true}))
+//
+app.get('/index',(req, res) => {
+    res.render('index.hbs')
+})
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000)
